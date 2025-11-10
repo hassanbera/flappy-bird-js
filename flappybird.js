4d026b1caf028072c1d2ba836eb9f5cde9bef849
hsnbera <hasanbera.kabadayi@serebellum.com>
2025-11-10 22:56:11 +0300

    bird's placed to the coordinates as primitive shape

--- flappybird.js
+++ flappybird.js
@@ -1,13 +1,28 @@
-
 // board
 let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
-window.onload = function(){
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+
+let bird = {
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight,
+};
+
+window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
-  board.width = boardWidth
+  board.width = boardWidth;
   context = board.getContext("2d");
-}
+
+  // draw flappy bird
+  context.fillStyle = "green";
+  context.fillRect(bird.x, bird.y, bird.width, bird.height);
+};
